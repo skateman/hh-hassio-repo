076bf939ee4f28c7d22228815c01aff750b98b44
Dávid Halász <skateman@skateman.eu>
2023-12-28 21:07:31 +0100

    dnsrobocert: drop timezone support

--- dnsrobocert/run.sh
+++ dnsrobocert/run.sh
@@ -4,14 +4,4 @@ mkdir -p /config/letsencrypt
 
 export HTTPS_PROXY="$(bashio::config 'proxy')"
 
-if [ -n "$TIMEZONE" ]; then
-  if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]; then
-    echo "Setting timezone to $TIMEZONE"
-    cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
-    echo "$TIMEZONE" > /etc/timezone
-  else
-    echo "$TIMEZONE does not exist"
-  fi
-fi
-
 exec dnsrobocert -c /config/config.yml -d /config/letsencrypt
